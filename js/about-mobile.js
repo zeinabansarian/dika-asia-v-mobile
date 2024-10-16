b66b89b908b3401dbe443fc08f465401975bf41d
Zeinab <zeinabansarian6280@gmail.com>
2024-10-16 10:57:32 +0330

    add load more

--- js/about-mobile.js
+++ js/about-mobile.js
@@ -51,3 +51,43 @@ const viewGallery = new Swiper('.viewGallery', {
     // And if we need scrollbar
    
   });
+
+
+  
+  let loadMore = document.querySelector(".loadMore");
+  let currentItems = 3;
+  const elementList = [...document.querySelectorAll(".projectGalleryBox")];
+  console.log(elementList[currentItems-1]);
+  if (elementList[currentItems-1]) {
+
+      elementList[currentItems-1].insertAdjacentElement("beforeend",loadMore)
+  }
+  if (currentItems < elementList.length) {
+    loadMore.addEventListener("click", function (e) {
+      const elementList = [...document.querySelectorAll(".projectGalleryBox")];
+      loadMore.classList.add("show-loader");
+
+      if (elementList[currentItems+2]) {
+          
+          elementList[currentItems+2].insertAdjacentElement("beforeend",loadMore)
+      }
+
+      for (let i = currentItems; i < currentItems + 3; i++) {
+          
+        setTimeout(() => {
+          e.target.classList.remove("show-loader");
+          if (elementList[i]) {
+            elementList[i].style.display = "block";
+          }
+        }, 0);
+      }
+      currentItems += 3;
+      if (currentItems >= elementList.length) {
+        loadMore.classList.add("loaded");
+      }
+    });
+  } else {
+    loadMore.classList.add("loaded");
+  }
+  
+  
